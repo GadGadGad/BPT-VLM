ce8e2b4c3eb5543430752806edfea7e3711d0571
Gad <faigar1004@gmail.com>
2025-05-08 22:52:44 +0700

    Update temporatory codes

--- model/Shallow_Prompt_CLIP_PGD.py
+++ model/Shallow_Prompt_CLIP_PGD.py
@@ -368,12 +368,12 @@ class PromptCLIP_Shallow:
     @torch.no_grad()
     def _evaluate_clip_baseline(self):
         print("--- Evaluating CLIP Baseline (once) ---")
-        self.model.to(self.dtype)
         pattern_prompt = [f"a photo of a {c.replace('_', ' ')}" for c in self.classes]
         tokenized_pattern_prompts = torch.cat([clip.tokenize(pattern_prompt)]).to(self.device)
 
         text_features_baseline = self.model.encode_text(tokenized_pattern_prompts).type(self.dtype)
         text_features_baseline = text_features_baseline / text_features_baseline.norm(dim=-1, keepdim=True)
+
 
         correct = 0.
         total = 0.
@@ -381,10 +381,10 @@ class PromptCLIP_Shallow:
 
         original_parallel_status = self.parallel
         self.parallel = False 
-    
+
         for batch in tqdm(self.test_loader, desc="Evaluating CLIP Baseline with 'a photo of a {c}'", leave=False):
             images, labels = self.parse_batch(batch) 
-            images = images.to(self.dtype)
+            
             image_features_baseline = self.model.encode_image(images).type(self.dtype)
             image_features_baseline = image_features_baseline / image_features_baseline.norm(dim=-1, keepdim=True)
 
