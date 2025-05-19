2276a4220842d9a89e485b9f6c7f908d22422ffa
Gad <faigar1004@gmail.com>
2025-05-19 20:10:52 +0700

    [Update] Update bug patches

--- scripts.sh
+++ scripts.sh
@@ -15,12 +15,29 @@ python BBT_VL_Shallow_PGD.py --task_name CIFAR10 \
                                      --opt shallow_cma \
                                      --pgd_test \
                                      --adv_train \
-                                     --pgd_test_num_iter 10 \
+                                     --pgd_test_num_iter 20 \
                                      --adv_train_num_iter 20 \
+                                     --adv_train_all \
                                      --parallel
-python BBT_VL_Shallow_PGD.py --task_name CIFAR10 \
-                                     --opt shallow_cma \
-                                     --pgd_test \
-                                     --pgd_test_num_iter 10 \
-                                     --adv_train_num_iter 20 \
-                                     --parallel
+                                    
+# python BBT_VL_Shallow_PGD.py --task_name CIFAR10 \
+#                                      --opt shallow_cma \
+#                                      --pgd_test \
+#                                      --adv_train \
+#                                      --pgd_test_num_iter 10 \
+#                                      --adv_train_num_iter 20 \
+#                                      --parallel
+# python BBT_VL_Shallow_PGD.py --task_name CIFAR10 \
+#                                      --opt shallow_cma \
+#                                      --pgd_test \
+#                                      --pgd_test_num_iter 10 \
+#                                      --adv_train_num_iter 20 \
+#                                      --parallel
+# python BBT_VL_Shallow_PGD.py --task_name CIFAR10 \
+#                                      --opt shallow_cma \
+#                                      --pgd_test \
+#                                      --adv_train \
+#                                      --pgd_test_num_iter 10 \
+#                                      --adv_train_num_iter 20 \
+#                                      --adv_train_all \
+#                                      --parallel
