895f39f61ce04409d01c9af9577b3f5343d8a624
Gad <faigar1004@gmail.com>
2025-05-19 19:23:01 +0700

    [Update] Update bug patches

--- scripts.sh
+++ scripts.sh
@@ -15,7 +15,7 @@ python BBT_VL_Shallow_PGD.py --task_name CIFAR10 \
                                      --opt shallow_cma \
                                      --pgd_test \
                                      --pgd_test_num_iter 20 \
-                                     --adv_train_num_iter 20 \
+                                     --adv_train_num_iter 20
 python BBT_VL_Shallow_PGD.py --task_name CIFAR10_PGD \
                                      --opt shallow_cma \
                                      --parallel
