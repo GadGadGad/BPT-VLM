01b29ede346ec77bcdc9a37c0b86db55ce635776
Gad <faigar1004@gmail.com>
2025-05-21 21:08:32 +0700

    [Update] Update bug patches

--- scripts.sh
+++ scripts.sh
@@ -4,40 +4,49 @@ python BBT_VL_Shallow_PGD.py --task_name CIFAR10 \
                                      --adv_train \
                                      --pgd_test_num_iter 20 \
                                      --adv_train_num_iter 20 \
+                                     --adv_train_all \
+                                     --parallel
+                                    
+python BBT_VL_Shallow_PGD.py --task_name CIFAR10 \
+                                     --opt shallow_cma \
+                                     --pgd_test \
+                                     --adv_train \
+                                     --pgd_test_num_iter 10 \
+                                     --adv_train_num_iter 20 \
                                      --parallel
 python BBT_VL_Shallow_PGD.py --task_name CIFAR10 \
                                      --opt shallow_cma \
                                      --pgd_test \
-                                     --pgd_test_num_iter 20 \
+                                     --pgd_test_num_iter 10 \
                                      --adv_train_num_iter 20 \
                                      --parallel
 python BBT_VL_Shallow_PGD.py --task_name CIFAR10 \
                                      --opt shallow_cma \
                                      --pgd_test \
                                      --adv_train \
-                                     --pgd_test_num_iter 20 \
+                                     --pgd_test_num_iter 10 \
                                      --adv_train_num_iter 20 \
                                      --adv_train_all \
                                      --parallel
-                                    
+
 # python BBT_VL_Shallow_PGD.py --task_name CIFAR10 \
 #                                      --opt shallow_cma \
 #                                      --pgd_test \
 #                                      --adv_train \
 #                                      --pgd_test_num_iter 10 \
-#                                      --adv_train_num_iter 20 \
+#                                      --adv_train_num_iter 30 \
 #                                      --parallel
 # python BBT_VL_Shallow_PGD.py --task_name CIFAR10 \
 #                                      --opt shallow_cma \
 #                                      --pgd_test \
 #                                      --pgd_test_num_iter 10 \
-#                                      --adv_train_num_iter 20 \
+#                                      --adv_train_num_iter 30 \
 #                                      --parallel
 # python BBT_VL_Shallow_PGD.py --task_name CIFAR10 \
 #                                      --opt shallow_cma \
 #                                      --pgd_test \
 #                                      --adv_train \
 #                                      --pgd_test_num_iter 10 \
-#                                      --adv_train_num_iter 20 \
+#                                      --adv_train_num_iter 30 \
 #                                      --adv_train_all \
 #                                      --parallel
