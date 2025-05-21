7d6c4391d650e9254989be5c4b1c14280bf3c5df
Gad <faigar1004@gmail.com>
2025-05-21 21:09:34 +0700

    [Update] Update bug patches

--- scripts.sh
+++ scripts.sh
@@ -4,7 +4,6 @@ python BBT_VL_Shallow_PGD.py --task_name CIFAR10 \
                                      --adv_train \
                                      --pgd_test_num_iter 20 \
                                      --adv_train_num_iter 20 \
-                                     --adv_train_all \
                                      --parallel
                                     
 python BBT_VL_Shallow_PGD.py --task_name CIFAR10 \
@@ -20,14 +19,6 @@ python BBT_VL_Shallow_PGD.py --task_name CIFAR10 \
                                      --pgd_test_num_iter 10 \
                                      --adv_train_num_iter 20 \
                                      --parallel
-python BBT_VL_Shallow_PGD.py --task_name CIFAR10 \
-                                     --opt shallow_cma \
-                                     --pgd_test \
-                                     --adv_train \
-                                     --pgd_test_num_iter 10 \
-                                     --adv_train_num_iter 20 \
-                                     --adv_train_all \
-                                     --parallel
 
 # python BBT_VL_Shallow_PGD.py --task_name CIFAR10 \
 #                                      --opt shallow_cma \
@@ -41,12 +32,4 @@ python BBT_VL_Shallow_PGD.py --task_name CIFAR10 \
 #                                      --pgd_test \
 #                                      --pgd_test_num_iter 10 \
 #                                      --adv_train_num_iter 30 \
-#                                      --parallel
-# python BBT_VL_Shallow_PGD.py --task_name CIFAR10 \
-#                                      --opt shallow_cma \
-#                                      --pgd_test \
-#                                      --adv_train \
-#                                      --pgd_test_num_iter 10 \
-#                                      --adv_train_num_iter 30 \
-#                                      --adv_train_all \
 #                                      --parallel
