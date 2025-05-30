4d44f01a28da3d797e3451d2d15f54fe0a397ff8
Gad <faigar1004@gmail.com>
2025-05-30 17:39:54 +0700

    [Updated] Update scripts.sh for server

--- scripts.sh
+++ scripts.sh
@@ -1,28 +1,28 @@
-python BBT_VL_Shallow_PGD_kaggle.py \
+python BBT_VL_Shallow_PGD.py \
     --task_name CIFAR10 \
     --adv_train \
     --adv_train_sample_ratio 0.5 \
     --pgd_test
 
-# python BBT_VL_Shallow_PGD_kaggle.py \
+# python BBT_VL_Shallow_PGD.py \
 #     --task_name CIFAR10 \
 #     --adv_train \
 #     --adv_train_attack_prompt_type "perturbed" \
 #     --pgd_test
 
-# python BBT_VL_Shallow_PGD_kaggle.py \
+# python BBT_VL_Shallow_PGD.py \
 #     --task_name CIFAR10 \
 #     --adv_train \
 #     --adv_train_attack_prompt_type "perturbed" \
 #     --adv_train_sample_ratio 0.5 \
 #     --pgd_test
 
-# python BBT_VL_Shallow_PGD_kaggle.py \
+# python BBT_VL_Shallow_PGD.py \
 #     --task_name CIFAR10 \
 #     --adv_train \
 #     --adv_train_attack_prompt_type "constant" \
 #     --pgd_test
-# python BBT_VL_Shallow_PGD_kaggle.py \
+# python BBT_VL_Shallow_PGD.py \
 #     --task_name CIFAR10 \
 #     --adv_train \
 #     --adv_train_attack_prompt_type "constant" \
