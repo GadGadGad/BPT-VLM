90ac15640c59eeb445e725e292d11051ca26a8db
Gad <faigar1004@gmail.com>
2025-06-08 16:41:23 +0700

    [Updated] Update initial prompt

--- scripts.sh
+++ scripts.sh
@@ -1,42 +1,63 @@
-python BBT_VL_Shallow_PGD.py \
-    --task_name CIFAR10 \
-    --adv_train \
-    --adv_train_all \
-    --adv_train_attack_type "gaussian" \
-    --adv_train_sample_ratio 0.1 \
-    --pgd_test
-python BBT_VL_Shallow_PGD.py \
-    --task_name CIFAR10 \
-    --adv_train \
-    --adv_train_all \
-    --adv_train_attack_type "gaussian" \
-    --adv_train_sample_ratio 0.3 \
-    --pgd_test
-python BBT_VL_Shallow_PGD.py \
-    --task_name CIFAR10 \
-    --adv_train \
-    --adv_train_all \
-    --adv_train_attack_type "gaussian" \
-    --adv_train_sample_ratio 0.5 \
-    --pgd_test
-python BBT_VL_Shallow_PGD.py \
-    --task_name CIFAR10 \
-    --adv_train \
-    --adv_train_all \
-    --adv_train_attack_type "gaussian" \
-    --adv_train_sample_ratio 0.7 \
-    --pgd_test
-python BBT_VL_Shallow_PGD.py \
-    --task_name CIFAR10 \
-    --adv_train \
-    --adv_train_all \
-    --adv_train_attack_type "gaussian" \
-    --adv_train_sample_ratio 0.9 \
-    --pgd_test
-python BBT_VL_Shallow_PGD.py \
-    --task_name CIFAR10 \
-    --adv_train \
-    --adv_train_all \
-    --adv_train_attack_type "gaussian" \
-    --adv_train_sample_ratio 1.0 \
-    --pgd_test
+python BBT_VL_Shallow_PGD.py --task_name CIFAR10 \
+                                    --opt shallow_cma \
+                                    --pgd_test \
+                                    --initial_prompt_text "a photo of a" \
+                                    --learned_prompt_pos "prefix"
+python BBT_VL_Shallow_PGD.py --task_name CIFAR10 \
+                                    --opt shallow_cma \
+                                    --pgd_test \
+                                    --initial_prompt_text "a photo of a" \
+                                    --learned_prompt_pos "middle"
+python BBT_VL_Shallow_PGD.py --task_name CIFAR10 \
+                                    --opt shallow_cma \
+                                    --pgd_test \
+                                    --initial_prompt_text "a photo of a" \
+                                    --learned_prompt_pos "suffix"
+# python BBT_VL_Shallow_PGD.py --task_name CIFAR10 \
+#                                     --opt shallow_cma \
+#                                     --adv_train \
+#                                     --adv_train_all \
+#                                     --adv_train_sample_ratio 0.5 \
+#                                     --pgd_test \
+#                                     --initial_prompt_text "a photo of a" \
+#                                     --learned_prompt_pos "prefix"
+# python BBT_VL_Shallow_PGD.py --task_name CIFAR10 \
+#                                     --opt shallow_cma \
+#                                     --adv_train \
+#                                     --adv_train_all \
+#                                     --adv_train_sample_ratio 0.5 \
+#                                     --pgd_test \
+#                                     --initial_prompt_text "a photo of a" \
+#                                     --learned_prompt_pos "middle"
+# python BBT_VL_Shallow_PGD.py --task_name CIFAR10 \
+#                                     --opt shallow_cma \
+#                                     --adv_train \
+#                                     --adv_train_all \
+#                                     --adv_train_sample_ratio 0.5 \
+#                                     --pgd_test \
+#                                     --initial_prompt_text "a photo of a" \
+#                                     --learned_prompt_pos "suffix"
+# python BBT_VL_Shallow_PGD.py --task_name CIFAR10 \
+#                                     --opt shallow_cma \
+#                                     --adv_train \
+#                                     --adv_train_all \
+#                                     --adv_train_sample_ratio 1.0 \
+#                                     --pgd_test \
+#                                     --initial_prompt_text "a photo of a" \
+#                                     --learned_prompt_pos "prefix"
+# python BBT_VL_Shallow_PGD.py --task_name CIFAR10 \
+#                                     --opt shallow_cma \
+#                                     --adv_train \
+#                                     --adv_train_all \
+#                                     --adv_train_sample_ratio 1.0 \
+#                                     --pgd_test \
+#                                     --initial_prompt_text "a photo of a" \
+#                                     --learned_prompt_pos "middle"
+# python BBT_VL_Shallow_PGD.py --task_name CIFAR10 \
+#                                     --opt shallow_cma \
+#                                     --adv_train \
+#                                     --adv_train_all \
+#                                     --adv_train_sample_ratio 1.0 \
+#                                     --pgd_test \
+#                                     --initial_prompt_text "a photo of a" \
+#                                     --learned_prompt_pos "suffix"
