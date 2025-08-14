bf2f7e2f3c2799dc45b65171ac0b5fbd40219afb
Gad <faigar1004@gmail.com>
2025-08-14 16:13:03 +0700

    [Update] Clean up

--- dataset/convert.py
+++ dataset/convert.py
@@ -3,8 +3,8 @@ import os
 import shutil
 
 
-source_path = "/kaggle/working/BPT-VLM/dataset/caltech101"
-target_path = "/kaggle/working/BPT-VLM/dataset/caltech101_Gen"
+source_path = "./FGVCAircraft"
+target_path = "./FGVCAircraft_Gen"
 # image_train_source_path = "cars_train"
 # image_test_source_path = "cars_test"
 
@@ -12,11 +12,10 @@ if not os.path.exists(target_path):
     os.makedirs(target_path)
 
 
-f = json.load(open(source_path+"/split_zhou_Caltech101.json","r",encoding="utf-8"))
-# print(f)
+f = json.load(open(source_path+"/split_zhou_FGVCAircraft.json","r",encoding="utf-8"))
+print(f)
 if not os.path.exists(target_path + "/image_data"):
     os.makedirs(target_path + "/image_data")
-
 for i in range(len(f["train"])):
     f["train"][i][0]=f["train"][i][0].replace(" ","_")
     f["train"][i][-1]=f["train"][i][-1].replace(" ","_")
@@ -27,7 +26,7 @@ for i in range(len(f["train"])):
 
     if not os.path.exists(target_path+"/image_data/"+f["train"][i][-1]):
         os.makedirs(target_path+"/image_data/"+f["train"][i][-1])
-    shutil.copy(source_path+"/101_ObjectCategories/"+f["train"][i][0], target_path+"/image_data/"+f["train"][i][-1]+"/"+f["train"][i][0].split("/")[-1])
+    shutil.copy(source_path+"/fgvc-aircraft-2013b/data/images/"+f["train"][i][0], target_path+"/image_data/"+f["train"][i][-1]+"/"+f["train"][i][0].split("/")[-1])
     f["train"][i][0] = f["train"][i][-1] + "/" + f["train"][i][0].split("/")[-1]
 
 for i in range(len(f["test"])):
@@ -40,7 +39,7 @@ for i in range(len(f["test"])):
 
     if not os.path.exists(target_path+"/image_data/"+f["test"][i][-1]):
         os.makedirs(target_path+"/image_data/"+f["test"][i][-1])
-    shutil.copy(source_path+"/101_ObjectCategories/"+f["test"][i][0], target_path+"/image_data/"+f["test"][i][-1]+"/"+f["test"][i][0].split("/")[-1])
+    shutil.copy(source_path+"/fgvc-aircraft-2013b/data/images/"+f["test"][i][0], target_path+"/image_data/"+f["test"][i][-1]+"/"+f["test"][i][0].split("/")[-1])
     f["test"][i][0] = f["test"][i][-1] + "/" + f["test"][i][0].split("/")[-1]
 
 json.dump(f,open(target_path+"/split.json","w"),indent=4)
